b6c0a86b31587faa54f68a440539c2f07b459cf2
koehlert <tim.koehler.net@gmail.com>
2023-12-08 19:26:14 +0100

    Add: animation

--- src/shooter/launcher.rs
+++ src/shooter/launcher.rs
@@ -1,18 +1,51 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, math::vec3};
 use crate::shooter::TriggerShotEvent;
 use bevy_rapier3d::prelude::*;
+
+const BALL_RADIUS :f32 = 0.08;
+const BALL_OFFSET : Vec3 = vec3(0., -0.2, -1.);
+#[derive(Component)]
+pub struct ReloadedBall;
+
+pub fn reload (
+	mut timer : ResMut<super::CooldownTimer>,
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+	transform: Query<Entity, With<Camera3d>>
+) {
+	if timer.timer.finished() && !timer.timer.paused(){
+		timer.timer.pause();
+		timer.timer.reset();
+		spawn_ball(&mut commands, &mut meshes, &mut materials, transform.single());
+	}
+}
 
 pub fn launch_ball (
 	mut event_reader : EventReader<TriggerShotEvent>,
 	mut commands: Commands,
-	mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
 	mut timer : ResMut<super::CooldownTimer>,
+	play_animation: Res<super::PlayAnimationSystem>,
+	mut ball_query: Query<(Entity, &mut GravityScale, &mut Transform), With<ReloadedBall>>
 ) {
 	for event in event_reader.read() {
-		if timer.timer.finished() {
-			spawn_ball(&mut commands, &mut meshes, &mut materials, &event);
+		if timer.timer.paused() {
+			info!("Launch!");
+			timer.timer.unpause();
+			// spawn_ball(&mut commands, &mut meshes, &mut materials, &event);
+			let (ball, mut gravity, mut transform) = ball_query.single_mut();
+			gravity.0 = 1.;
+			transform.translation = event.direction.translation + event.direction.up()*BALL_OFFSET.y + event.direction.back()*BALL_OFFSET.z;
+			commands.entity(ball).insert((
+				Velocity {
+					linvel: event.direction.forward() * event.magnitude,
+					angvel: Vec3::new(0.2, 0.0, 0.0),
+				},
+			));
+			commands.entity(ball).remove::<Parent>();
+			commands.entity(ball).remove::<ReloadedBall>();
 			timer.timer.reset();
+			commands.run_system(play_animation.0);
 		}
 		
 	}
@@ -22,26 +55,24 @@ fn spawn_ball(
 	commands: &mut Commands,
 	meshes: &mut ResMut<Assets<Mesh>>,
     materials: &mut ResMut<Assets<StandardMaterial>>,
-	event: &TriggerShotEvent
+	parent: Entity
 ){
-	let tf = event.direction.translation + (event.direction.forward()*1.2);
-	commands.spawn(PbrBundle {
-		mesh: meshes.add(Mesh::from(shape::UVSphere { radius: 0.5, ..default() })),
+	info!("Spawn");
+	let ball_id = commands.spawn(PbrBundle {
+		mesh: meshes.add(Mesh::from(shape::UVSphere { radius: BALL_RADIUS, ..default() })),
 		material: materials.add(Color::rgb_u8(124, 144, 255).into()),
-		transform: event.direction.with_translation(tf),
+		transform: Transform {translation: BALL_OFFSET, ..default()},
 		..default()
 	}).insert((
 		RigidBody::Dynamic,
-		GravityScale(1.0),
-		Velocity {
-			linvel: event.direction.forward() * event.magnitude,
-			angvel: Vec3::new(0.2, 0.0, 0.0),
-		},
-		Collider::ball(0.5),
+		Collider::ball(BALL_RADIUS),
 		super::BallMarker,
+		GravityScale(0.),
 		Restitution {
 			coefficient: 0.7,
 			combine_rule: CoefficientCombineRule::Average,
-		}
-	));
+		},
+		ReloadedBall
+	)).id();
+	commands.entity(parent).add_child(ball_id);
 }
--- src/shooter/mod.rs
+++ src/shooter/mod.rs
@@ -1,11 +1,13 @@
 use std::time::Duration;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, ecs::system::SystemId};
 pub struct ShooterPlugin;
 
 mod camera;
 mod inputs;
 mod launcher;
+mod animation;
+use animation::*;
 
 #[derive(Event)]
 pub struct TriggerShotEvent {
@@ -17,16 +19,23 @@ pub struct CooldownTimer{
 	timer: Timer
 }
 
+#[derive(Resource)]
+pub struct Animations(Vec<Handle<AnimationClip>>);
+
+#[derive(Resource)]
+pub struct PlayAnimationSystem(SystemId);
+
 #[derive(Component)]
 pub struct BallMarker;
 
 impl Plugin for ShooterPlugin {
 	fn build(&self, app: &mut App) {
-		app.add_systems(Startup, camera::spawn_camera)
-			.add_systems(Update, (inputs::keyboard_movements, inputs::shoot_trigger, ))
-			.add_systems(Update, (inputs::keyboard_movements, inputs::shoot_trigger, launcher::launch_ball, tick_cooldown_timer))
+		app.add_systems(Startup, (camera::spawn_camera, load_animations))
+			.add_systems(Update, (inputs::keyboard_movements, inputs::shoot_trigger, launcher::launch_ball, launcher::reload, tick_cooldown_timer, setup_scene_once_loaded))
 			.add_event::<TriggerShotEvent>()
-			.insert_resource(CooldownTimer {timer: Timer::new(Duration::from_secs_f32(0.5), TimerMode::Once)});
+			.insert_resource(CooldownTimer {timer: Timer::new(Duration::from_secs_f32(3.), TimerMode::Once)});
+		let id = app.world.register_system(animation::replay_animation);
+		app.insert_resource(PlayAnimationSystem(id));
 	}
 }
 
