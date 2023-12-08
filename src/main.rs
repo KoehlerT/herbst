545f85c456bc1264bf7b45f78c859e49d4d45583
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-12-08 16:25:53 +0100

    add ambient light

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use bevy_rapier3d::prelude::*;
 
 mod shooter;
 mod floor;
+mod tree;
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, WorldInspectorPlugin::new()))
-		.add_plugins((shooter::ShooterPlugin, floor::FloorPlugin))
+		.add_plugins((shooter::ShooterPlugin, floor::FloorPlugin, tree::TreePlugin))
         .add_plugins((RapierPhysicsPlugin::<NoUserData>::default(), RapierDebugRenderPlugin::default()))
         .add_systems(Startup, setup)
         .run();
@@ -23,11 +24,17 @@ fn setup(
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
-            intensity: 1500.0,
+            intensity: 2500.0,
             shadows_enabled: true,
             ..default()
         },
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
+    });
+
+    // ambient light
+    commands.insert_resource(AmbientLight {
+        color: Color::ORANGE_RED,
+        brightness: 0.06,
     });
 }
