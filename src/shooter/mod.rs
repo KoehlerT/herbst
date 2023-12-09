544194930f26906fc53b7646abb2682bf37476bd
koehlert <tim.koehler.net@gmail.com>
2023-12-09 19:56:59 +0100

    Add: prevent movement in start and endscreen

--- src/shooter/mod.rs
+++ src/shooter/mod.rs
@@ -9,6 +9,8 @@ mod launcher;
 mod animation;
 use animation::*;
 
+use crate::game::AppState;
+
 #[derive(Event)]
 pub struct TriggerShotEvent {
 	direction: Transform,
@@ -34,7 +36,7 @@ pub struct ShooterPower(f32);
 impl Plugin for ShooterPlugin {
 	fn build(&self, app: &mut App) {
 		app.add_systems(Startup, (camera::spawn_camera, load_animations))
-			.add_systems(Update, (inputs::keyboard_movements, inputs::shoot_trigger, launcher::launch_ball, launcher::reload, tick_cooldown_timer, setup_scene_once_loaded))
+			.add_systems(Update, ((inputs::keyboard_movements, inputs::shoot_trigger).run_if(in_state(AppState::InGame)), launcher::launch_ball, launcher::reload, tick_cooldown_timer, setup_scene_once_loaded))
 			.add_event::<TriggerShotEvent>()
 			.insert_resource(CooldownTimer {timer: Timer::new(Duration::from_secs_f32(3.), TimerMode::Once)})
 			.insert_resource(ShooterPower(15.0));
